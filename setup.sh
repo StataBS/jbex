36561ad12d0556a43ff4aeaadfb02e73fbedc669
NiklausBaltis <niklaus.baltisberger@bs.ch>
2021-11-23 09:14:37 +0100

    Version 0.1.0

--- setup.sh
+++ setup.sh
@@ -9,4 +9,10 @@ headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
 
+[theme]\n\
+primaryColor = '#070707'\n\
+backgroundColor = '#fcfcfc'\n\
+secondaryBackgroundColor = '#e3e3e3'\n\
+textColor = '#070707'\n\
+
 " > ~/.streamlit/config.toml
